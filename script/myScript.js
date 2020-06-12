f8acb10dec901cfee8d148f4c0ec1653f267a491
Kathrin Schey <kathrin.dostal@gmail.com>
2020-06-12 13:32:13 +0200

    some styles

--- script/myScript.js
+++ script/myScript.js
@@ -2,18 +2,26 @@ $(document).ready(function()
 {
     let movie = JSON.parse(info);
     console.log(movie);
+    var count = 0;
 
     for (let i = 0; i < movie.length; i++)
     {
-        document.getElementById("container").innerHTML += `<div class="content">
+        document.getElementById("container").innerHTML += 
+                    `<div id="${i}" class="content">
                         <div>
                             <img src="${movie[i].img}" alt="Banner">
                         </div>
                         <div>
                             <h4>${movie[i].titel}</h4>
                             <p>${movie[i].info}</p>
-                            <button>${movie[i].like}</button>
+                            <div class="likeOutput">
+                                <button class="buttOn"><img src="${movie[i].like}"></button>
+                                <h2 class="output"></h2>
+                            </div>
                         </div>
-                    </div>`;
-    }
+                    </div>`; 
+    }    
+
+    
+
 });
